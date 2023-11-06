9db71ec81b8f4e2a8aebac3f50311115c1dff09a
Richard Chou <konami99@hotmail.com>
2023-11-06 23:22:49 +1100

    template message

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 import {
   ClientConfig,
   MessageAPIResponseBase,
@@ -8,6 +9,7 @@ import {
   MiddlewareConfig,
   webhook,
 } from '@line/bot-sdk';
+import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 
 const clientConfig: ClientConfig = {
   channelAccessToken: 'hgLFz1jGdEYCoNuVK3zIV4nxaDp6K3/tgLnfNK3pvf2ig8tEuSyN8B9KRW9RCGIZH3QrxWzhrk3h/ig0GLyarkemnkJfWNalAVFI0hbGn7vnBeSo3IzkcE6e7CB7NNkWH4ih1Cqgtc2sPXYGpuwR/AdB04t89/1O/w1cDnyilFU=' || '',
@@ -18,7 +20,7 @@ const middlewareConfig: MiddlewareConfig = {
   channelSecret: '5071e5e75ecc7ea0e9db8eb67f1909b0' || '',
 };
 
-const client = new messagingApi.MessagingApiClient(clientConfig);
+const lineClient = new messagingApi.MessagingApiClient(clientConfig);
 
 export const lambdaHandler = async (event: any): Promise<any> => {
   try {
@@ -26,37 +28,79 @@ export const lambdaHandler = async (event: any): Promise<any> => {
     console.log(event["events"][0]);
     const events = event["events"];
     const firstEvent = events[0];
-    /*
-    {
-      type: 'message',
-      message: {
-        type: 'text',
-        id: '480375402586702258',
-        quoteToken: 'syduXPNp_HkqIt3nRT-ELX4shBfg_eOFNJz2hbqtkxPVXSEBpflG-evKM1032u9gm29IJSXVSeehgF7zJnuh-rQ4_eAZ_5A5DknpwWmJrSZrRO5pzvPC01K3Bjb0Nq3FVrVynDLt4iQ0cvS10_jeXQ',
-        text: 'The '
-      },
-      webhookEventId: '01HEETQC0ZXDV9EEA7RBDSYBC4',
-      deliveryContext: { isRedelivery: true },
-      timestamp: 1699157356324,
-      source: { type: 'user', userId: 'Uf653f8e04aae9441cc3d8e6a41cfe28a' },
-      replyToken: 'cad44f6a83f94635854065c79e07d14b',
-      mode: 'active'
+
+    switch (firstEvent.type) {
+      case 'message':
+        const message = firstEvent.message.text.split(' ')[1];
+        const replyToken = firstEvent.replyToken;
+        
+        await lineClient.replyMessage({
+          replyToken: replyToken as string,
+          messages: [
+            {
+              type: 'template',
+              altText: 'Confirm alt text',
+              template: {
+                type: 'buttons',
+                text: `要什麼時候提醒您 "${message}"?`,
+                actions: [
+                  { 
+                    type: 'datetimepicker',
+                    label: '點我選擇時間',
+                    data: message,
+                    mode: 'datetime'
+                  },
+                ],
+              },
+            }
+          ]
+        });
+      case 'postback':
+        console.log('postback');
+        const text = firstEvent.postback.data;
+        console.log(text);
+        const userId = firstEvent.source.userId;
+        
+        const aws_table_name = 'line-reminders';
+        const aws_local_config = {
+          //Provide details for local configuration
+        }
+        const aws_remote_config = {
+          accessKeyId: 'AKIAQFFDKRTMFDQMB624',
+          secretAccessKey: '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',
+          region: 'us-west-2',
+        }
+        
+        //AWS.config.update(aws_remote_config);
+        const client = new DynamoDBClient({
+          region: 'us-west-2' as string,
+        });
+        const dbDocClient = DynamoDBDocumentClient.from(client);
+        const params = {
+          TableName: 'line-reminders',
+          Item: {
+            user_id: userId,
+            created_at: 199999
+          }
+        }
+        const data = await dbDocClient.send(new PutCommand(params))
+        console.log(data)
+        /*
+        const docClient = new AWS.DynamoDB.DocumentClient();
+        const item = {
+          user_id: userId,
+          created_at: new Date().getTime().toString(),
+        }
+        var params = {
+          TableName: aws_table_name,
+          Item: item
+        };
+        */
+        //docClient.put(params)
     }
-    */
+  
+
     
-
-    if (firstEvent.type ==='message' && firstEvent.source.type === 'user' && firstEvent.message.text === '@toast') {
-      const message = firstEvent.message.text;
-      const replyToken = firstEvent.replyToken;
-
-      await client.replyMessage({
-        replyToken: replyToken as string,
-        messages: [{
-          type: 'text',
-          text: '您需要提醒什麼事情?',
-        }],
-      });
-    }
 
     return {
       statusCode: 200,

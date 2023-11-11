e909a965a10ffd8b9dcf5f557c6d8fef8cc04dbd
Richard Chou <konami99@hotmail.com>
2023-11-12 08:43:33 +1100

    added dynamodb

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient, UpdateItemCommand } from "@aws-sdk/client-dynamodb";
 import { Client } from "@upstash/qstash";
+import { DateTime } from "luxon";
 import {
   ClientConfig,
   MessageAPIResponseBase,
@@ -9,45 +10,99 @@ import {
   MiddlewareConfig,
   webhook,
 } from '@line/bot-sdk';
-import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
+import { DynamoDBDocumentClient, PutCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb';
+
 
 const clientConfig: ClientConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN as string,
 };
 
 /*
-const middlewareConfig: MiddlewareConfig = {
-  channelAccessToken: 'hgLFz1jGdEYCoNuVK3zIV4nxaDp6K3/tgLnfNK3pvf2ig8tEuSyN8B9KRW9RCGIZH3QrxWzhrk3h/ig0GLyarkemnkJfWNalAVFI0hbGn7vnBeSo3IzkcE6e7CB7NNkWH4ih1Cqgtc2sPXYGpuwR/AdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '5071e5e75ecc7ea0e9db8eb67f1909b0' || '',
+const clientConfig: ClientConfig = {
+  channelAccessToken: '',
 };
 */
 
 const lineClient = new messagingApi.MessagingApiClient(clientConfig);
 
+
 const qstashClient = new Client({
   token: process.env.QSTASH_TOKEN as string,
 });
+
+
+/*
+const qstashClient = new Client({
+  token: '',
+});
+*/
 
 export const lambdaHandler = async (event: any): Promise<any> => {
   try {
     console.log(event);
 
-    if (event?.type === 'qstash') {
+    /*
+    production
+    */
+    const jsonObject = JSON.parse(event.body);
+    
+
+    /*
+    local
+    */
+    //const events = event["events"];
+    //const firstEvent = events[0];
+    //const jsonObject = firstEvent;
+    
+    if (jsonObject?.type === 'qstash') {
       lineClient.pushMessage({
-        to: event.userId,
+        to: jsonObject.user_id,
         messages: [
           {
             type: 'text',
-            text: event.text
+            text: `提醒您: ${jsonObject.text}`
           }
         ]
       })
+
+      const params = {
+        TableName: 'line-reminders',
+        Key: {
+          user_id: { 'S': jsonObject.user_id }, // The key of the item to update
+          created_at: { 'N': `${jsonObject.created_at}` },
+        },
+        UpdateExpression: 'SET #status = :newStatus', // Define the update expression
+        ExpressionAttributeNames: {
+          '#status': 'status', // Replace 'status' with the attribute to update
+        },
+        ExpressionAttributeValues: {
+          ':newStatus': { 'S': 'sent' }, // Define the new value for the attribute
+        },
+      };
+
+      const client = new DynamoDBClient({
+        region: 'us-west-2' as string,
+      });
+      //const dbDocClient = DynamoDBDocumentClient.from(client);
+      //const data = await dbDocClient.send(new UpdateCommand(params));
+      await client.send(new UpdateItemCommand(params))
     } else {
       /*
       body: '{"destination":"U1f7351b944cb4b8c52529beeff107717","events":[{"type":"postback","postback":{"data":"你好嗎","params":{"datetime":"2023-11-08T21:21"}},"webhookEventId":"01HEQ76MN3HQAGCWK1B7SZFVNS","deliveryContext":{"isRedelivery":false},"timestamp":1699438875261,"source":{"type":"user","userId":"Uf653f8e04aae9441cc3d8e6a41cfe28a"},"replyToken":"9636527508bd4885999d1698450a2188","mode":"active"}]}'
       */
-      const jsonObject = JSON.parse(event.body);
+      
+      /*
+      production
+      */
       const firstEvent = jsonObject.events[0];
+      
+
+      /*
+      local
+      */
+      //const events = event["events"];
+      //const firstEvent = events[0];
+      
       switch (firstEvent.type) {
         case 'message':
           const message = firstEvent.message.text.split(' ')[1];
@@ -75,37 +130,47 @@ export const lambdaHandler = async (event: any): Promise<any> => {
           });
         case 'postback':
           const text = firstEvent.postback.data;
+          const time = firstEvent.postback.params.datetime //2023-11-05T19:48
           const userId = firstEvent.source.userId;
-          
+
+
           /*
-          const aws_remote_config = {
-            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-            region: process.env.AWS_REGION,
-          }
+          time = '2023-11-05T19:48'
           */
-          /*
+          const timeWithZone = DateTime.fromFormat(time, "yyyy-MM-dd'T'HH:mm", { zone: 'Australia/Sydney'} )
+          
           const client = new DynamoDBClient({
             region: 'us-west-2' as string,
           });
           const dbDocClient = DynamoDBDocumentClient.from(client);
+          const created_at = parseInt(new Date().getTime().toString())
           const params = {
             TableName: 'line-reminders',
             Item: {
               user_id: userId,
-              created_at: 399999
+              created_at,
+              scheduled_at: timeWithZone.toSeconds(),
+              status: 'scheduled',
+              message: text,
             }
           }
-          */
-          //const data = await dbDocClient.send(new PutCommand(params))
-          /*
+          
+
+          const data = await dbDocClient.send(new PutCommand(params))
+          
+          
           const res = await qstashClient.publishJSON({
-            url: "https://qstash.upstash.io/v2/publish/reminders-tw",
+            topic: "reminders-tw",
+            notBefore: timeWithZone.toSeconds(),
             body: {
-              reminder: text,
+              type: 'qstash',
+              user_id: userId,
+              created_at,
+              text,
             },
           });
-          */
+          
+          
           await lineClient.replyMessage({
             replyToken: firstEvent.replyToken as string,
             messages: [
